de130e719dc1f99aae2630c3935c04d097a0787e
Christopher Tolbert <cmtolbert1@gmail.com>
2013-09-26 15:26:21 -0700

    Add for loop to iterate thru each cell

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+function navCells(){
+alert("Go to navCells");
+
+for(i=1; i<=9; ++i)
+	
+		document.getElementById("sqr"+i).onlcick=
+	function(){
+		alert("you clicked me");
+			}
+	}
+var turn = 0;
 function playBox(){
-/*	var turn = 0;
-	if ((turn%2==0) && (turn<10))
+	//alert("Hello World")
+	if ((turn%2==0) && (turn<9))
 		event.target.innerHTML="X";
-	else if (!turn%2==0) && (turn<10)
+	else if (!(turn%2==0) && (turn<9))
 		event.target.innerHTML="O";
 	else 
-		alert("Game over no more moves");
-*/
-	event.target.innerHTML="X"
+		alert("Game over no more moves")
+	turn++
+
+//	event.target.innerHTML="X"
 
 	}
 
@@ -18,8 +30,12 @@ function startPlayAgain() {
 function navExit() {
 	location.href="https://google.com"	
 }
+
+
 /*
 var winners = ((sqr1==sqr2==sqr3) || (sqr4==sqr5==sqr6) || (sqr7==sqr8==sqr9) || (sqr1==sqr4==sqr7) || (sqr2==sqr5==sqr8)|| (sqr3==sqr6==sqr9) || (sqr1==sqr5==sqr9) || (sqr3==sqr5==sqr7));
+
+winners = [[sqr1,sqr2,sqr3],[sqr4,sqr5,sqr6],[sqr7,sqr8,sqr9],[sqr1,sqr4,sqr7],[sqr2,sqr5,sqr8],[sqr3,sqr6,sqr9],[sqr1,sqr5,sqr9],[sqr3,sqr5,sqr7]];
 
 function checkWin() {
 	if winners(true) && sqr1.value=="X"
