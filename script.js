5b2ce3918a2e3b9809cf3523f4caa0c8098d06d0
Christopher Tolbert <cmtolbert1@gmail.com>
2013-09-26 19:24:37 -0700

    Finished basic game

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+var singArray =["","","","","","","","",""]; //could be:0,0,0 or null, null, null
 function navCells(){
-alert("Go to navCells");
+/*alert("Go to navCells");*/
 
-for(i=1; i<=9; ++i)
-	
-		document.getElementById("sqr"+i).onlcick=
-	function(){
-		alert("you clicked me");
-			}
-	}
+}
 var turn = 0;
 function playBox(){
-	//alert("Hello World")
+	/*alert("Hello World")*/
+	if (event.target.innerHTML !="")
+		return;
+
 	if ((turn%2==0) && (turn<9))
 		event.target.innerHTML="X";
 	else if (!(turn%2==0) && (turn<9))
@@ -19,7 +17,33 @@ function playBox(){
 		alert("Game over no more moves")
 	turn++
 
-//	event.target.innerHTML="X"
+	for(i=1; i<=9; ++i)
+	{		
+	singArray[i-1]= //or for sqr1_2 cellArray[r-1][c-1]
+		document.getElementById("sqr"+i).innerHTML;
+
+	}
+	//tests to see who wins, if any
+
+		if(singArray[0] == singArray[1] && singArray[1] == singArray[2] && singArray[0] != "")
+			alert(singArray[0] + " is the champion!");
+
+		if(singArray[3] == singArray[4] && singArray[4] == singArray[5] && singArray[3] != "")
+			alert(singArray[3] + " is the champion!");
+		
+		if(singArray[6] == singArray[7] && singArray[7] == singArray[8] && singArray[6] != "")
+			alert(singArray[6] + " is the champion!");
+		if(singArray[0] == singArray[3] && singArray[3] == singArray[3] && singArray[0] != "")
+			alert(singArray[0] + " is the champion!");
+		if(singArray[1] == singArray[4] && singArray[4] == singArray[7] && singArray[1] != "")
+			alert(singArray[1] + " is the champion!");
+		if(singArray[2] == singArray[5] && singArray[5] == singArray[8] && singArray[2] != "")
+			alert(singArray[2] + " is the champion!");
+		if(singArray[0] == singArray[4] && singArray[4] == singArray[8] && singArray[0] != "")
+			alert(singArray[0] + " is the champion!");
+		
+		if(singArray[2] == singArray[4] && singArray[4] == singArray[6] && singArray[2] != "")
+			alert(singArray[6] + " is the champion!");
 
 	}
 
@@ -31,6 +55,7 @@ function navExit() {
 	location.href="https://google.com"	
 }
 
+//var r1 = [sqr1,sqr2,sqr3];
 
 /*
 var winners = ((sqr1==sqr2==sqr3) || (sqr4==sqr5==sqr6) || (sqr7==sqr8==sqr9) || (sqr1==sqr4==sqr7) || (sqr2==sqr5==sqr8)|| (sqr3==sqr6==sqr9) || (sqr1==sqr5==sqr9) || (sqr3==sqr5==sqr7));
