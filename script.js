95f04290e24facd481277a2d54f680a4f08cd16d
Christopher Tolbert <cmtolbert1@gmail.com>
2013-09-27 18:42:15 -0700

    Added start popup & win popup

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-var singArray =["","","","","","","","",""]; //could be:0,0,0 or null, null, null
-function navCells(){
-/*alert("Go to navCells");*/
+//could be:0,0,0 or null, null, null
+var singArray =["","","","","","","","",""]; 
 
+//declare a variable starting with 1
+var popupOpacity = 1.0;
+
+
+function pageLoaded(){
+/*alert("Go to navCells");*/
+//document.getElementById("win").style.opacity=0.2;
+
+//setTimeout("alert('One secod has elapsed.');", 1000);
+
+//This will set the timeout to 3secs while calling a function that we have to define later
+setTimeout("timeout();", 1000);
 }
+
+//This function says that on timeout it will find all id's and the style of opacity will ==popupOpacity will decrement by .1 as long as the opacity is over -.1; couldnt get it to work with >= 0. Decrements every 100ms
+function timeout() {
+	document.getElementById("start").style.opacity = popupOpacity;
+	popupOpacity -= .1;
+	if(popupOpacity >= -0.1)
+			setTimeout("timeout();", 100);
+
+	if(popupOpacity <= 0);
+ 		document.getElementById("start").style.left="-50000px";
+}
+
+/*function hidePopup (){
+	popup.style.display = "none";
+}
+
+function showPopup (){
+	popup.style.display = "inline-block";
+}
+*/
 var turn = 0;
 function playBox(){
 	/*alert("Hello World")*/
@@ -24,15 +55,20 @@ function playBox(){
 
 	}
 	//tests to see who wins, if any
+	var champ;
 
 		if(singArray[0] == singArray[1] && singArray[1] == singArray[2] && singArray[0] != "")
-			alert(singArray[0] + " is the champion!");
+			document.getElementById("win").innerHTML=style.display = inline-block; // = (singArray[0] + " is the champion!");
 
 		if(singArray[3] == singArray[4] && singArray[4] == singArray[5] && singArray[3] != "")
-			alert(singArray[3] + " is the champion!");
+			{champ = singArray[3];
+				document.getElementById("win").innerHTML=champ + " is the champion!";
+				document.getElementById("win").style.display="inline-block";
+			}
 		
 		if(singArray[6] == singArray[7] && singArray[7] == singArray[8] && singArray[6] != "")
 			alert(singArray[6] + " is the champion!");
+
 		if(singArray[0] == singArray[3] && singArray[3] == singArray[6] && singArray[0] != "")
 			alert(singArray[0] + " is the champion!");
 		if(singArray[1] == singArray[4] && singArray[4] == singArray[7] && singArray[1] != "")
@@ -55,6 +91,8 @@ function navExit() {
 	location.href="https://google.com"	
 }
 
+
+//document.getElementById("alert").innerHTML=
 //var r1 = [sqr1,sqr2,sqr3];
 
 /*
@@ -79,5 +117,3 @@ if (turn%2==0) {
 
 };
 */
-
-/**/
